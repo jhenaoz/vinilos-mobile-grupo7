86ab13cb204fb481a29b1b716ba7fdf1f05516de
Alexander Mora Ruiz <a.morar2@uniandes.edu.co>
2023-04-25 21:06:23 -0500

    update navigation bar

--- app/src/main/java/co/misw4203/grupo7/vinilos/MainActivity.kt
+++ app/src/main/java/co/misw4203/grupo7/vinilos/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_albums, R.id.navigation_artists
+                R.id.navigation_home, R.id.navigation_albums, R.id.navigation_artists, R.id.navigation_collections
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
---
+++ app/src/main/java/co/misw4203/grupo7/vinilos/ui/collections/CollectionsViewModel.kt
@@ -0,0 +1,13 @@
+package co.misw4203.grupo7.vinilos.ui.collections
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CollectionsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is collections Fragment"
+    }
+    val text: LiveData<String> = _text
+}
