de5269883abf9919a70e2930bd376e6ac1ec539d
Andres Camilo Cardenas <liderdesarrollo@tht.company>
2023-05-13 14:54:04 -0500

    Cache images

--- app/src/main/java/co/misw4203/grupo7/vinilos/repositories/PerformerRepository.kt
+++ app/src/main/java/co/misw4203/grupo7/vinilos/repositories/PerformerRepository.kt
@@ -1,20 +1,37 @@
 package co.misw4203.grupo7.vinilos.repositories
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
 import android.util.Log
+import co.misw4203.grupo7.vinilos.database.BandDao
+import co.misw4203.grupo7.vinilos.database.MusicianDao
 import co.misw4203.grupo7.vinilos.models.Band
 import co.misw4203.grupo7.vinilos.models.Musician
 import co.misw4203.grupo7.vinilos.network.CacheManager
 import co.misw4203.grupo7.vinilos.network.NetworkServiceAdapter
 
-class PerformerRepository (val application: Application) {
+class PerformerRepository (val application: Application, private val musicianDao: MusicianDao, private val bandDao: BandDao) {
 
     suspend fun refreshDataMusicians(): List<Musician>{
-        return NetworkServiceAdapter.getInstance(application).getMusicians()
+        var cached = musicianDao.getMusicians()
+        return if(cached.isNullOrEmpty()){
+            val cm = application.baseContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if( cm.activeNetworkInfo?.type != ConnectivityManager.TYPE_WIFI && cm.activeNetworkInfo?.type != ConnectivityManager.TYPE_MOBILE){
+                emptyList()
+            } else  NetworkServiceAdapter.getInstance(application).getMusicians()
+        } else cached
+
     }
 
     suspend fun refreshDataBands(): List<Band>{
-        return NetworkServiceAdapter.getInstance(application).getBands()
+        var cached = bandDao.getBands()
+        return if(cached.isNullOrEmpty()){
+            val cm = application.baseContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if( cm.activeNetworkInfo?.type != ConnectivityManager.TYPE_WIFI && cm.activeNetworkInfo?.type != ConnectivityManager.TYPE_MOBILE){
+                emptyList()
+            } else NetworkServiceAdapter.getInstance(application).getBands()
+        } else cached
     }
 
     suspend fun refreshDataMusicianById(id: Int): Musician {
