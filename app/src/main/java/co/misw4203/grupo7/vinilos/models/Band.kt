2ba52509372999b712f0017b4f47f43fbc2f286c
accardenasc1 <ac.cardenasc1@uniandes.edu.co>
2023-05-13 19:58:25 -0500

    Feature hu04 with optimizations (#22)
    * first commit repository and cache
    
    * second commit repository and cache
    
    * Cache images
    
    * Fix repository
    
    * microoptimizations
    
    ---------
    
    Co-authored-by: Andres Camilo Cardenas <liderdesarrollo@tht.company>

--- app/src/main/java/co/misw4203/grupo7/vinilos/models/Band.kt
+++ app/src/main/java/co/misw4203/grupo7/vinilos/models/Band.kt
@@ -1,10 +1,9 @@
 package co.misw4203.grupo7.vinilos.models
 
-import java.util.Date
-
 data class Band(
     override val id: Int,
     override val name: String,
     override val image: String,
     override val description: String,
+    override val albums: List<Album>,
     val creationDate: String): Performer
--- app/src/main/java/co/misw4203/grupo7/vinilos/models/Musician.kt
+++ app/src/main/java/co/misw4203/grupo7/vinilos/models/Musician.kt
@@ -1,11 +1,10 @@
 package co.misw4203.grupo7.vinilos.models
 
-import java.util.Date
-
 data class Musician(
     override val id: Int,
     override val name: String,
     override val image: String,
     override val description: String,
+    override val albums: List<Album>,
     val birthDate: String
 ): Performer
