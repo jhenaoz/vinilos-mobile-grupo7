fdcec252ffe0a523d151b8bc4cd48fd6112a37be
Alexander Mora Ruiz <a.morar2@uniandes.edu.co>
2023-05-14 23:02:08 -0500

    eliminar carpeta apk

--- apk/generated/source/kapt/debug/androidx/databinding/DataBinderMapperImpl.java
+++
@@ -1,7 +0,0 @@
-package androidx.databinding;
-
-public class DataBinderMapperImpl extends MergedDataBinderMapper {
-  DataBinderMapperImpl() {
-    addMapper(new co.misw4203.grupo7.vinilos.DataBinderMapperImpl());
-  }
-}
