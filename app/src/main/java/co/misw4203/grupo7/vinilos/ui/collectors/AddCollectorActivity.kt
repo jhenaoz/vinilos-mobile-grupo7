602147f2db06ff2b813ecf55e03976166c81261c
aaperezmoncada <aa.perezm12@uniandes.edu.co>
2023-05-21 16:30:41 -0500

    Oculta botón crear coleccionista

--- app/src/main/java/co/misw4203/grupo7/vinilos/ui/collectors/AddCollectorActivity.kt
+++ app/src/main/java/co/misw4203/grupo7/vinilos/ui/collectors/AddCollectorActivity.kt
@@ -2,37 +2,18 @@ package co.misw4203.grupo7.vinilos.ui.collectors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
-import android.widget.RadioGroup
-import android.widget.Toast
 import co.misw4203.grupo7.vinilos.R
 import co.misw4203.grupo7.vinilos.brokers.VolleyBroker
 import com.google.android.material.button.MaterialButton
-import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
-import org.json.JSONObject
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Locale
 
 class AddCollectorActivity : AppCompatActivity() {
-    private lateinit var inputReleaseDate: TextInputEditText
-    private lateinit var inputReleaseDateLayout: TextInputLayout
-    private lateinit var datePickerReleaseDate: MaterialDatePicker<Long>
     private lateinit var inputTitle: TextInputEditText
     private lateinit var inputTitleLayout: TextInputLayout
-    private lateinit var inputCover: TextInputEditText
-    private lateinit var inputCoverLayout: TextInputLayout
-    private lateinit var radioGroupGenre: RadioGroup
-    private lateinit var radioGroupRecordLabel: RadioGroup
-    private lateinit var inputDescription: TextInputEditText
-    private lateinit var inputDescriptionLayout: TextInputLayout
-
+    private lateinit var inputReleaseTelephone: TextInputEditText
+    private lateinit var inputReleaseEmailLayout: TextInputEditText
     private lateinit var buttonAddCollector: MaterialButton
-
     private lateinit var volleyBroker: VolleyBroker
 
 
@@ -44,30 +25,14 @@ class AddCollectorActivity : AppCompatActivity() {
 
         inputTitle = findViewById(R.id.collector_name_layout)
         inputTitleLayout = findViewById(R.id.collector_title_layout)
-        inputReleaseDate = findViewById(R.id.collector_release_telephone)
-        inputReleaseDateLayout = findViewById(R.id.collector_release_email)
+        inputReleaseTelephone = findViewById(R.id.collector_release_telephone)
+        inputReleaseEmailLayout = findViewById(R.id.collector_release_email)
         buttonAddCollector= findViewById(R.id.button_add_collector)
 
-        // Input release date
-        val datePickerBuilder = MaterialDatePicker.Builder.datePicker()
-        datePickerBuilder.setTitleText("Seleccionar fecha")
-        datePickerReleaseDate = datePickerBuilder.build()
-
-        inputReleaseDate.setOnClickListener {
-            datePickerReleaseDate.show(supportFragmentManager, "date_picker")
-        }
-
-        datePickerReleaseDate.addOnPositiveButtonClickListener { selectedDate ->
-            val calendar = Calendar.getInstance()
-            calendar.timeInMillis = selectedDate
-
-            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.US)
-            val dateString = dateFormat.format(calendar.time)
-            inputReleaseDate.setText(dateString)
-        }
+    }
 
         // Make inputs required
-        fun makeInputRequired(inputLayout: TextInputLayout, inputEditText: TextInputEditText) {
+        /*fun makeInputRequired(inputLayout: TextInputLayout, inputEditText: TextInputEditText) {
             inputEditText.addTextChangedListener(object : TextWatcher {
                 override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
@@ -82,57 +47,5 @@ class AddCollectorActivity : AppCompatActivity() {
                 override fun afterTextChanged(s: Editable?) {}
             })
         }
-
-        makeInputRequired(inputTitleLayout, inputTitle)
-        makeInputRequired(inputReleaseDateLayout, inputReleaseDate)
-        makeInputRequired(inputCoverLayout, inputCover)
-        makeInputRequired(inputReleaseDateLayout, inputReleaseDate)
-        makeInputRequired(inputDescriptionLayout, inputDescription)
-
-        // Button add collector
-        buttonAddCollector.setOnClickListener {
-            val title = inputTitle.text.toString()
-            val cover = inputCover.text.toString()
-            val releaseDate = inputReleaseDate.text.toString()
-            val genre = when (radioGroupGenre.checkedRadioButtonId) {
-                R.id.radio_button_classical -> getText(R.string.label_classical)
-                R.id.radio_button_salsa -> getText(R.string.label_salsa)
-                R.id.radio_button_rock -> getText(R.string.label_rock)
-                R.id.radio_button_folk -> getText(R.string.label_folk)
-                else -> getText(R.string.label_classical)
-            }
-            val recordLabel = when (radioGroupRecordLabel.checkedRadioButtonId) {
-                R.id.radio_button_sony_music -> getText(R.string.label_sony_music)
-                R.id.radio_button_emi -> getText(R.string.label_emi)
-                R.id.radio_button_discos_fuentes -> getText(R.string.label_discos_fuentes)
-                R.id.radio_button_elektra -> getText(R.string.label_elektra)
-                R.id.radio_button_fania_records -> getText(R.string.label_fania_records)
-                else -> getText(R.string.label_sony_music)
-            }
-
-            val description = inputDescription.text.toString()
-
-            val postParams = mapOf<String, Any>(
-                "name" to title,
-                "cover" to cover,
-                "releaseDate" to releaseDate.replace("\\", ""),
-                "description" to description,
-                "genre" to genre,
-                "recordLabel" to recordLabel
-            )
-
-            volleyBroker.instance.add(VolleyBroker.postRequest("collectors", JSONObject(postParams),
-                { _ ->
-                    Log.d("TAG", it.toString())
-                    Toast.makeText(this.applicationContext, "Albúm creado correctamente", Toast.LENGTH_LONG).show()
-                    finish()
-                },
-                {
-                    Log.d("TAG", it.toString())
-                    Toast.makeText(this.applicationContext, "Error al crear albúm", Toast.LENGTH_LONG).show()
-
-                }
-            ))
-        }
+*/
     }
-}
